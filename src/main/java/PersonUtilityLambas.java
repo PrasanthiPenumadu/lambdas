631189b24994e806e36faf620cc88f65b1aa161c
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-05 15:18:54 -0400

    Committing Search criteria code in Local ,Anonymous Class and with a lambda expression.

--- src/main/java/PersonUtilityLambas.java
+++ src/main/java/PersonUtilityLambas.java
@@ -2,29 +2,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class PersonUtilityLambas {
-//    public static void printPersonsOlderThan(List<Person> lPeople, int age) {
-//        for (Person p : lPeople) {
-//            if (p.getAge() >= age) {
-//                p.printPerson();
-//            }
-//        }
-//        lPeople.stream().filter(person -> (person.getAge()>age)).collect(Collectors.toList());
-//    }
-//    public static void printPersonsWithinAgeRange(
-//            List<Person> lPeople, int low, int high) {
-//        for (Person p : lPeople) {
-//            if (low <= p.getAge() && p.getAge() < high) {
-//                p.printPerson();
-//            }
-//        }
-//    }
-//    public static void printPersons(List<Person> lPeople, CheckPerson tester) {
-//        for (Person p : lPeople) {
-//            if (tester.test(p)) {
-//                p.printPerson();
-//            }
-//        }
-//    }
         public  Integer personOlder(List<Person> lPeople, int age){
 
             return lPeople.stream().filter(person -> (person.getAge()>age)).collect(Collectors.toList()).size();
